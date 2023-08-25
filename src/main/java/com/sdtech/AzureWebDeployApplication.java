679a86f290d644759854c7bf60a8849cb2ac2e6f
Anuj <anuj.1999@gmail.com>
2023-08-25 15:11:27 +0530

    This is the first commit for Azure web app

---
+++ src/main/java/com/sdtech/AzureWebDeployApplication.java
@@ -0,0 +1,25 @@
+package com.sdtech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@SpringBootApplication
+public class AzureWebDeployApplication {
+
+	@GetMapping("/message")
+	public String demoMessage()
+	{
+		return  "Hi! This the message from Azure Web App";
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Application Started !!!!!");
+		SpringApplication.run(AzureWebDeployApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sdtech/AzureWebDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sdtech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureWebDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
